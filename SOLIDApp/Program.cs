0f011bfd40838152f0cc8eaeb687ba7cd4979298
Arzu Yıldız <arzuuyildizz@hotmail.com>
2022-06-01 18:55:12 +0300

    SRP

--- SOLIDApp/Program.cs
+++ SOLIDApp/Program.cs
@@ -1,15 +1,20 @@
 ﻿
 
 //SRP — Single Responsibility Principle (Tek Sorumluluk Prensibi)
+
 class Customer
 {
     public void Login(string username, string passwd) { }
     public void Register(string username, string passwd, string email)
     {
-        this.SendEmail("Kaydınız alınmıştır...");
-    }
-    void SendEmail(string message) {
-        Console.WriteLine(message);
+        EmailSender emailSender = new EmailSender();
+        emailSender.SendEmail("Kaydınız alınmıştır...");
     }
 }
+
+class EmailSender
+{
+    public void SendEmail(string message) { }
+}
+
 
